f415e25c10723c906e2b50ea0478ce88851cd12e
Guillermo <guilassa0809@gmail.com>
2017-05-10 16:13:49 +0200

    Eliminación de fichero de la herramienta Yeoman
    Eliminación de los fichero restantes de la herramienta que utilizamos
    al comienzo debido al merge que hemos realizado en el anterior commit.

--- app/php/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define("HOSTNAME","localhost");
-define("USERNAME","root");
-define("PASSWORD","root");
-define("DATABASE","ingenieriadesoftware");
-
-$dbhandle=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE) or die("Unable to Connect DB");
-
-?>
